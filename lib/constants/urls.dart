ddb91b0254d98ff742b5d20769ed8fe3d4db7513
glowin2004 <glowinshabu2004@gmail.com>
2025-02-13 14:30:59 +0530

    change

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,7 +1,7 @@
 // const String baseUrl = "https://c3cmjnm3-8001.inc1.devtunnels.ms/";
 // const String ImageUrl = "https://c3cmjnm3-8001.inc1.devtunnels.ms";
-const String baseUrl = "https://c3cmjnm3-8001.inc1.devtunnels.ms/";
-const String ImageUrl = "https://c3cmjnm3-8001.inc1.devtunnels.ms";
+const String baseUrl = "https://84wv8dp3-8001.inc1.devtunnels.ms/";
+const String ImageUrl = "https://84wv8dp3-8001.inc1.devtunnels.ms";
 
 class Urls {
   static const String userRegister = "${baseUrl}register/user/";
